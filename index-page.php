6ffbe6e34746e06a6d2e00cc054187662df8341e
Jorge Saud <jorgelsaud@gmail.com>
2016-08-09 23:31:02 -0400

    some fixes

--- index-page.php
+++ index-page.php
@@ -6,6 +6,18 @@ get_header(); ?>
 
 <!-- section -->
 	<div class="contenedorInicio">
-		<?php putRevSlider('slider');?>
+	<div class="headerIndex">
+		<div class="Logo">
+			<?php the_custom_logo()?>
+		</div>
+		<div id="dl-menu" class="dl-menuwrapper">
+			<button class="dl-trigger">Open Menu</button>
+			<?php callMenu('main-menu')?>
+		</div><!-- /dl-menuwrapper -->
+
+	</div>
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+		<?php the_content()?>
+	<?php endwhile;endif;?>
 	</div>
 	<?php get_footer(); ?>
