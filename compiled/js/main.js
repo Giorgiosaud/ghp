2980c5ad5d8ff9f66a404152bcd03b9f41068843
Jorge Saud <jorgelsaud@gmail.com>
2016-08-14 02:25:15 -0400

    fix fancybox

--- compiled/js/main.js
+++ compiled/js/main.js
@@ -3,6 +3,32 @@ jQuery(document).ready(function($){
 		animationClasses : { classin : 'dl-animate-in-4', classout : 'dl-animate-out-4' },
 		backLabel:'Atras'
 	});
+	/* change revapi1 to whatever API name is being used for your slider */
+	var api = revapi6;
+	/* no need to edit below unless you want to change the default FancyBox settings */
+	api.on('revolution.slide.onloaded', function() {
+		jQuery(this).find('.fancybox').each(function() {
+			var $this = jQuery(this);
+			if(!$this.is('a')) $this = $this.removeClass('fancybox').find('a');
+			$this.addClass('fancybox').attr('rel', 'gallery').fancybox({
+				/* begin FancyBox options */
+				width: 'auto',
+				height: 'auto',
+				autoSize: true,
+				aspectRatio: true,
+				fitToView: true,
+				autoCenter: true,
+				scrolling: 'no',
+				onClosed: function() {
+					api.revresume();
+				}
+			}).on('click', function() {
+				api.revpause();
+			});
+
+		});
+
+	});
 });
 
 //# sourceMappingURL=main.js.map
