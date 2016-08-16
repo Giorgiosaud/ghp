0f07e2b372fcd766e0f625b1c795f24d98818140
Jorge Saud <jorgelsaud@gmail.com>
2016-08-16 13:48:04 -0400

    fix doble maps

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -24,7 +24,7 @@ jQuery(document).ready(function($){
 		}
 	});
 	$('input[name|="Producto"]').change(function() {
-		$('.TipoCliente').animateCss('fade-in-down-big');
+		$('.TipoCliente').animateCss('fade-out-down');
 	});
 
 });
