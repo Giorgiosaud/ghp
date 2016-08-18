47487f1d62cb244bc2b2a3a40c7a11af61c1f3c6
Jorge Saud <jorgelsaud@gmail.com>
2016-08-17 22:21:50 -0400

    m

--- page-contactos.php
+++ page-contactos.php
@@ -17,44 +17,28 @@ get_header(); ?>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-xs-12 col-md-6 contenidoInterno__Contenido">
-				<div class="BotonesContactos">
-					<div class="BotonContacto BotonesContactos__Email">
-						<div class="Boton_Icono BotonesContactos__Email__Icono"><img src="<?php echo get_template_directory_uri();?>/img/email.png" class="img-fluid" alt="email"></div>
-						<div class="BotonContacto_Contenido BotonesContactos__Email__Correo"><a href="<?php the_field('email','option')?>"><?php the_field('email','option')?></a></div>
-					</div>
-					<div class="BotonContacto BotonesContactos__Telefono">
-						<div class="Boton_Icono BotonesContactos__Telefono__Icono"><img src="<?php echo get_template_directory_uri();?>/img/telefono.png" class="img-fluid" alt="Telefono"></div>
-						<div class="BotonContacto_Contenido BotonesContactos__Telefono__Correo"><a href="tel<?php the_field('telefono_sin_formato','option')?>"><?php the_field('telefono_con_formato','option')?></a></div>
-					</div>
-					<div class="BotonContacto BotonesContactos__Direccion">
-						<div class="Boton_Icono BotonesContactos__Direccion__Icono"><img src="<?php echo get_template_directory_uri();?>/img/direccion.png" class="img-fluid" alt="Direccion"></div>
-						<div class="BotonContacto_Contenido BotonesContactos__Direccion__Correo"><a href="<?php the_field('direccion_link','option')?>"><?php the_field('direccion','option')?></a></div>
-					</div>
-				</div>
-				<?php the_content()?>
-			</div>
+			
 			<div class="col-xs-12 col-md-6 contenidoInterno__Formulario">
 				<div class="contenidoInterno__Formulario__Titulo">
-					<h4><?php _e('¿Que Tipo de Producto Necesita?')?></h4>
+					<h4><?php _e('¿Que tipo de producto necesita?')?></h4>
 				</div>
 				<div class="contenidoInterno__Formulario__selectorTipoProducto">
 					<label class="form-check-inline">
 						<input class="form-check-input" type="radio" name="Producto" id="inlineRadio1" value="V"> 	
-						<span>Ventanas</span>
+						<span>Ventanas PVC</span>
 					</label>
 					<label class="form-check-inline">
-						<input class="form-check-input" type="radio" name="Producto" id="inlineRadio2" value="JV"><span>Jardines Verticales</span>
+						<input class="form-check-input" type="radio" name="Producto" id="inlineRadio2" value="JV"><span>Muros Verticales</span>
 					</label>
 				</div>
 				<div class="TipoCliente hidden">
 					<div class="contenidoInterno__Formulario__Titulo">
-						<h4><?php _e('¿Que Tipo de Cliente Eres?')?></h4>
+						<h4><?php _e('¿Que tipo de cliente eres?')?></h4>
 					</div>
 					<div class="contenidoInterno__Formulario__selectorTipoCliente">
 						<label class="form-check-inline">
 							<input class="form-check-input" type="radio" name="TipoDeCliente" id="inlineRadio3" value="DC"> 	
-							<span>Dueño de Casa</span>
+							<span>Dueño de casa</span>
 						</label>
 						<label class="form-check-inline">
 							<input class="form-check-input" type="radio" name="TipoDeCliente" id="inlineRadio4" value="E"><span>Especialista</span>
@@ -75,6 +59,23 @@ get_header(); ?>
 						<?php the_field('jardines_dueno_de_casa')?>
 					</div>
 				</div>
+			</div>
+			<div class="col-xs-12 col-md-6 contenidoInterno__Contenido">
+				<div class="BotonesContactos">
+					<div class="BotonContacto BotonesContactos__Email">
+						<div class="Boton_Icono BotonesContactos__Email__Icono"><img src="<?php echo get_template_directory_uri();?>/img/email.png" class="img-fluid" alt="email"></div>
+						<div class="BotonContacto_Contenido BotonesContactos__Email__Correo"><a href="<?php the_field('email','option')?>"><?php the_field('email','option')?></a></div>
+					</div>
+					<div class="BotonContacto BotonesContactos__Telefono">
+						<div class="Boton_Icono BotonesContactos__Telefono__Icono"><img src="<?php echo get_template_directory_uri();?>/img/telefono.png" class="img-fluid" alt="Telefono"></div>
+						<div class="BotonContacto_Contenido BotonesContactos__Telefono__Correo"><a href="tel<?php the_field('telefono_sin_formato','option')?>"><?php the_field('telefono_con_formato','option')?></a></div>
+					</div>
+					<div class="BotonContacto BotonesContactos__Direccion">
+						<div class="Boton_Icono BotonesContactos__Direccion__Icono"><img src="<?php echo get_template_directory_uri();?>/img/direccion.png" class="img-fluid" alt="Direccion"></div>
+						<div class="BotonContacto_Contenido BotonesContactos__Direccion__Correo"><a href="<?php the_field('direccion_link','option')?>"><?php the_field('direccion','option')?></a></div>
+					</div>
+				</div>
+				<?php the_content()?>
 			</div>
 		</div>
 	</div>
