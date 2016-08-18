c988f60dc85bd2e11022f56e941457661f17b4f3
Jorge Saud <jorgelsaud@gmail.com>
2016-08-17 22:29:29 -0400

    contact page

--- page-contactos.php
+++ page-contactos.php
@@ -15,10 +15,24 @@ get_header(); ?>
 			<div class="TituloInterno col-xs-12 col-md-5">
 				<?php the_title()?>
 			</div>
+			<div class="BotonesContactos BotonesContactos col-xs-12 col-md-5 col-md-push-1">
+					<div class="BotonContacto BotonesContactos__Email">
+						<div class="Boton_Icono BotonesContactos__Email__Icono"><img src="<?php echo get_template_directory_uri();?>/img/email.png" class="img-fluid" alt="email"></div>
+						<div class="BotonContacto_Contenido BotonesContactos__Email__Correo"><a href="<?php the_field('email','option')?>"><?php the_field('email','option')?></a></div>
+					</div>
+					<div class="BotonContacto BotonesContactos__Telefono">
+						<div class="Boton_Icono BotonesContactos__Telefono__Icono"><img src="<?php echo get_template_directory_uri();?>/img/telefono.png" class="img-fluid" alt="Telefono"></div>
+						<div class="BotonContacto_Contenido BotonesContactos__Telefono__Correo"><a href="tel<?php the_field('telefono_sin_formato','option')?>"><?php the_field('telefono_con_formato','option')?></a></div>
+					</div>
+					<div class="BotonContacto BotonesContactos__Direccion">
+						<div class="Boton_Icono BotonesContactos__Direccion__Icono"><img src="<?php echo get_template_directory_uri();?>/img/direccion.png" class="img-fluid" alt="Direccion"></div>
+						<div class="BotonContacto_Contenido BotonesContactos__Direccion__Correo"><a href="<?php the_field('direccion_link','option')?>"><?php the_field('direccion','option')?></a></div>
+					</div>
+				</div>
 		</div>
 		<div class="row">
 			
-			<div class="col-xs-12 col-md-6 contenidoInterno__Formulario">
+			<div class="col-xs-12 contenidoInterno__Formulario">
 				<div class="contenidoInterno__Formulario__Titulo">
 					<h4><?php _e('¿Que tipo de producto necesita?')?></h4>
 				</div>
@@ -59,23 +73,6 @@ get_header(); ?>
 						<?php the_field('jardines_dueno_de_casa')?>
 					</div>
 				</div>
-			</div>
-			<div class="col-xs-12 col-md-6 contenidoInterno__Contenido">
-				<div class="BotonesContactos">
-					<div class="BotonContacto BotonesContactos__Email">
-						<div class="Boton_Icono BotonesContactos__Email__Icono"><img src="<?php echo get_template_directory_uri();?>/img/email.png" class="img-fluid" alt="email"></div>
-						<div class="BotonContacto_Contenido BotonesContactos__Email__Correo"><a href="<?php the_field('email','option')?>"><?php the_field('email','option')?></a></div>
-					</div>
-					<div class="BotonContacto BotonesContactos__Telefono">
-						<div class="Boton_Icono BotonesContactos__Telefono__Icono"><img src="<?php echo get_template_directory_uri();?>/img/telefono.png" class="img-fluid" alt="Telefono"></div>
-						<div class="BotonContacto_Contenido BotonesContactos__Telefono__Correo"><a href="tel<?php the_field('telefono_sin_formato','option')?>"><?php the_field('telefono_con_formato','option')?></a></div>
-					</div>
-					<div class="BotonContacto BotonesContactos__Direccion">
-						<div class="Boton_Icono BotonesContactos__Direccion__Icono"><img src="<?php echo get_template_directory_uri();?>/img/direccion.png" class="img-fluid" alt="Direccion"></div>
-						<div class="BotonContacto_Contenido BotonesContactos__Direccion__Correo"><a href="<?php the_field('direccion_link','option')?>"><?php the_field('direccion','option')?></a></div>
-					</div>
-				</div>
-				<?php the_content()?>
 			</div>
 		</div>
 	</div>
