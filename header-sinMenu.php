398f848e0809403f87924f3ee29fd10bb6ba148e
Jorge Saud <jorgelsaud@gmail.com>
2016-08-17 22:40:48 -0400

    fix single margin in responsive

---
+++ header-sinMenu.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<!--	<meta name="description" content="<?php bloginfo('description'); ?>">-->
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<?php endif; ?>
+		<?php wp_head(); ?>
+		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+</head>
+
+<body <?php body_class(); ?>>
+
+<div class="headerInner container-fluid">
+	<div class="Logo">
+		<?php the_custom_logo()?>
+	</div>
+	<div class="boton">
+		<a href="tel:<?php the_field('telefono_sin_formato','option')?>" class="btn btn-primary btn-ghp btn-sm"><?php the_field('telefono_con_formato','option')?></a>
+	</div>
+	<div id="dl-menu" class="dl-menuwrapper">
+		<button class="dl-trigger">Open Menu</button>
+	</div><!-- /dl-menuwrapper -->
+
+</div>
---
+++ page-conSlider-sinMenu.php
@@ -0,0 +1,84 @@
+<?php 
+/*
+ * Template name: Pagina Con Slider Sin Menu
+ * */
+get_header('sinMenu'); ?>
+
+<div class="backgroundPicture" <?php if(has_post_thumbnail()):?>
+	style="background:url(<?php the_post_thumbnail_url('full')?>)">
+<?php else:?>
+	style="background:url(<?= get_template_directory_uri();?>/img/bg-image-inner.jpg)">
+<?php endif ?>
+<article class="container contenidoInterno">
+	<div class="row">
+		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<div class="TituloInterno col-xs-12 col-md-5">
+				<?php the_title()?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-md-6 contenidoInterno__Contenido">
+				<?php the_content()?>
+			</div>
+			<div class="col-xs-12 col-md-6 contenidoInterno__Foto">
+				<div class="contenidoInterno__Galeria">
+					<?php 
+
+					$images = get_field('slider');
+					$iterator=0;
+					if( $images ): ?>
+
+
+
+					<div id="Galeria" class="carousel slide" data-ride="carousel">
+						<div class="carousel-inner" role="listbox">
+							<?php foreach( $images as $index=>$image ):?>
+							<div class="carousel-item <?php if($index==0){echo 'active';}?>">
+								<a href="<?php echo $image['sizes']['large']; ?>" rel="gallery" class="fancybox" title="<?php echo $image['caption']; ?>">
+								<img src="<?php echo $image['sizes']['imagen_interna']; ?>" alt="<?php echo $image['alt']; ?>	">
+								</a>
+							</div>
+							<?php endforeach; ?>
+
+						</div>
+						<a class="left carousel-control" href="#Galeria" role="button" data-slide="prev">
+							<span class="icon-prev" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#Galeria" role="button" data-slide="next">
+							<span class="icon-next" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				<?php endif;?>
+				</div>
+			</div>
+		</div>
+	</div>
+</article>
+<?php endwhile; ?>
+<?php else: ?>
+	<article class="container contenidoInterno">
+		<div class="row">
+			<div class="TituloInterno col-xs-12 col-md-5">
+				<?php __('La Pagina Que Buscas No Existe','ghp')?>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 col-md-6 contenidoInterno__Contenido">
+				<?php __('Por favor ve al inicio haciendo click en nuestro logo','ghp')?>
+			</div>
+			<div class="col-xs-12 col-md-6 contenidoInterno__Foto">
+				<div class="contenidoInterno__ImagenyColores">
+					<div class="contenidoInterno__Imagen">
+						<img src="<?php get_template_directory_uri() ?>/img-bg-image-inner.jpg" class="img-fluid" alt="Imagen Interna">
+					</div>
+				</div>
+			</div>
+		</div>
+	</article>
+</div>
+
+
+<?php endif; ?>
+<?php get_footer(); ?>
