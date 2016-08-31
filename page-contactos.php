6f62d9be0ab42850be158fbdf6624178831e18d1
Jorge Saud <jorgelsaud@gmail.com>
2016-08-31 10:30:28 -0400

    fix centered content

--- page-contactos.php
+++ page-contactos.php
@@ -6,34 +6,38 @@ get_header(); ?>
 
 <div class="backgroundPicture" <?php if(has_post_thumbnail()):?>
 	style="background:url(<?php the_post_thumbnail_url('full')?>)">
-<?php else:?>
+	<?php else:?>
 	style="background:url(<?= get_template_directory_uri();?>/img/bg-image-inner.jpg)">
-<?php endif ?>
-<article class="container contenidoInterno">
-	<div class="row">
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-			<div class="col-xs-12 col-md-5 noMP">
-				<div class="TituloInterno">
-					<?php the_title()?>
+	<?php endif ?>
+	<article class="container contenidoInterno">
+		<div class="row">
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+			<div class="row">
+				<div class="col-xs-12 col-md-5 noMP">
+					<div class="TituloInterno">
+						<?php the_title()?>
+					</div>
 				</div>
 			</div>
-			<div class="BotonesContactos BotonesContactos col-xs-12 col-md-7">
-				<div class="BotonContacto BotonesContactos__Email">
-					<div class="Boton_Icono BotonesContactos__Email__Icono"><img src="<?php echo get_template_directory_uri();?>/img/email.png" class="img-fluid" alt="email"></div>
-					<div class="BotonContacto_Contenido BotonesContactos__Email__Correo"><a href="<?php the_field('email','option')?>"><?php the_field('email','option')?></a></div>
-				</div>
-				<div class="BotonContacto BotonesContactos__Telefono">
-					<div class="Boton_Icono BotonesContactos__Telefono__Icono"><img src="<?php echo get_template_directory_uri();?>/img/telefono.png" class="img-fluid" alt="Telefono"></div>
-					<div class="BotonContacto_Contenido BotonesContactos__Telefono__Correo"><a href="tel<?php the_field('telefono_sin_formato','option')?>"><?php the_field('telefono_con_formato','option')?></a></div>
-				</div>
-				<div class="BotonContacto BotonesContactos__Direccion">
-					<div class="Boton_Icono BotonesContactos__Direccion__Icono"><img src="<?php echo get_template_directory_uri();?>/img/direccion.png" class="img-fluid" alt="Direccion"></div>
-					<div class="BotonContacto_Contenido BotonesContactos__Direccion__Correo"><a href="<?php the_field('direccion_link','option')?>"><?php the_field('direccion','option')?></a></div>
+			<div class="row">
+				<div class="BotonesContactos BotonesContactos col-xs-12 col-md-7">
+					<div class="BotonContacto BotonesContactos__Email">
+						<div class="Boton_Icono BotonesContactos__Email__Icono"><img src="<?php echo get_template_directory_uri();?>/img/email.png" class="img-fluid" alt="email"></div>
+						<div class="BotonContacto_Contenido BotonesContactos__Email__Correo"><a href="<?php the_field('email','option')?>"><?php the_field('email','option')?></a></div>
+					</div>
+					<div class="BotonContacto BotonesContactos__Telefono">
+						<div class="Boton_Icono BotonesContactos__Telefono__Icono"><img src="<?php echo get_template_directory_uri();?>/img/telefono.png" class="img-fluid" alt="Telefono"></div>
+						<div class="BotonContacto_Contenido BotonesContactos__Telefono__Correo"><a href="tel<?php the_field('telefono_sin_formato','option')?>"><?php the_field('telefono_con_formato','option')?></a></div>
+					</div>
+					<div class="BotonContacto BotonesContactos__Direccion">
+						<div class="Boton_Icono BotonesContactos__Direccion__Icono"><img src="<?php echo get_template_directory_uri();?>/img/direccion.png" class="img-fluid" alt="Direccion"></div>
+						<div class="BotonContacto_Contenido BotonesContactos__Direccion__Correo"><a href="<?php the_field('direccion_link','option')?>"><?php the_field('direccion','option')?></a></div>
+					</div>
 				</div>
 			</div>
 		</div>
 		<div class="row">
-			
+
 			<div class="col-xs-12 col-md-8 contenidoInterno__Formulario noMP">
 				<div class="TituloInterno">
 					<?php _e('¿Que tipo de producto necesita?')?>
@@ -89,25 +93,25 @@ get_header(); ?>
 </article>
 <?php endwhile; ?>
 <?php else: ?>
-	<article class="container contenidoInterno">
-		<div class="row">
-			<div class="TituloInterno col-xs-12 col-md-5">
-				<?php __('La Pagina Que Buscas No Existe','ghp')?>
-			</div>
+<article class="container contenidoInterno">
+	<div class="row">
+		<div class="TituloInterno col-xs-12 col-md-5">
+			<?php __('La Pagina Que Buscas No Existe','ghp')?>
 		</div>
-		<div class="row">
-			<div class="col-xs-12 col-md-6 contenidoInterno__Contenido">
-				<?php __('Por favor ve al inicio haciendo click en nuestro logo','ghp')?>
-			</div>
-			<div class="col-xs-12 col-md-6 contenidoInterno__Foto">
-				<div class="contenidoInterno__ImagenyColores">
-					<div class="contenidoInterno__Imagen">
-						<img src="<?php get_template_directory_uri() ?>/img-bg-image-inner.jpg" class="img-fluid" alt="Imagen Interna">
-					</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-md-6 contenidoInterno__Contenido">
+			<?php __('Por favor ve al inicio haciendo click en nuestro logo','ghp')?>
+		</div>
+		<div class="col-xs-12 col-md-6 contenidoInterno__Foto">
+			<div class="contenidoInterno__ImagenyColores">
+				<div class="contenidoInterno__Imagen">
+					<img src="<?php get_template_directory_uri() ?>/img-bg-image-inner.jpg" class="img-fluid" alt="Imagen Interna">
 				</div>
 			</div>
 		</div>
-	</article>
+	</div>
+</article>
 </div>
 
 
